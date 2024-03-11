70b73dca4de52cc5f2f8deb90f898a288221b4bc
PatRider64 <83703900+PatRider64@users.noreply.github.com>
2024-03-11 16:35:07 +0100

    add create book endpoint

--- src/main/java/com/pucelj/restapidemo/controller/BookController.java
+++ src/main/java/com/pucelj/restapidemo/controller/BookController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,11 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+
+    @PostMapping("")
+    public Book create(@RequestBody Book book) {
+        return bookService.save(book);
+    }
 
     @GetMapping("")
     public List<Book> findAll() {
